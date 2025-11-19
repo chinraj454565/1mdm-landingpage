a093253e47380f8c2503da223c680cd977b581a1
Chinraj P <jarchin6@gmail.com>
2025-11-19 12:26:32 +0530

    success-section-changes

--- src/components/SuccessStories.js
+++ src/components/SuccessStories.js
@@ -1,5 +1,6 @@
 
 import { useState } from "react";
+import { useEffect } from "react";
 
 const IconArrowLeft = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"><path d="M19 12H5"></path><path d="M12 19l-7-7 7-7"></path></svg>
@@ -42,6 +43,13 @@ export const SuccessStories = () => {
     const prevStory = () => {
         setActiveIndex((prevIndex) => (prevIndex - 1 + totalStories) % totalStories);
     };
+
+    useEffect(() => {
+        const intervalId = setInterval(nextStory, 13000); 
+        
+        
+        return () => clearInterval(intervalId);
+    }, [activeIndex]);
 
     return (
         <section className="stories-section">
